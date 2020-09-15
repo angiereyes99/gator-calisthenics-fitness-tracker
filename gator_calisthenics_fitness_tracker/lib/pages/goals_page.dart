abc5953fde02909d46e6e393bd78585332c13833
Angelo Reyes <angeloreyes1180@yahoo.com>
2020-09-15 12:41:34 -0700

    goals page

--- gator_calisthenics_fitness_tracker/lib/pages/goals_page.dart
+++ gator_calisthenics_fitness_tracker/lib/pages/goals_page.dart
@@ -1,32 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:gator_calisthenics_fitness_tracker/utils/constants.dart';
 
-
 class GoalsPage extends StatefulWidget {
 
   static final String id = 'goals_page';
 
   @override
-  _GoalsPageState createState() => _GoalsPageState();
+  createState() => new GoalsPageState();
 }
 
-class _GoalsPageState extends State<GoalsPage> {
+class GoalsPageState extends State<GoalsPage> {
+  List<String> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    if(task.length > 0) {
+      setState(() => _todoItems.add(task));
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('Mark "${_todoItems[index]}" as done?'),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('CANCEL'),
+              onPressed: () => Navigator.of(context).pop()
+            ),
+            new FlatButton(
+              child: new Text('MARK AS DONE'),
+              onPressed: () {
+                _removeTodoItem(index);
+                Navigator.of(context).pop();
+              }
+            )
+          ]
+        );
+      }
+    );
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if(index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index], index);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText, int index) {
+    return new CheckboxListTile(
+      onChanged: null,
+      value: false,
+      title: new Text(
+        todoText,
+        style: TextStyle(color: Colors.black),
+      ),
+      //onTap: () => _promptRemoveTodoItem(index)
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
+      appBar: new AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.green,
+        title: new Text('Set Goals Here')
+      ),
       body: Container(
         decoration: primaryBackground,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Text('This is for Goals page')
-            ],
-          ),
-        ),
+        child: _buildTodoList(),
       ),
+      floatingActionButton: new FloatingActionButton(
+        backgroundColor: Colors.green[900],
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add task',
+        child: new Icon(
+          Icons.add,
+          color: Colors.black,
+        )
+      ),
+    );
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+      // MaterialPageRoute will automatically animate the screen entry, as well as adding
+      // a back button to close it
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Add a new task')
+            ),
+            body: new TextField(
+              autofocus: true,
+              onSubmitted: (val) {
+                _addTodoItem(val);
+                Navigator.pop(context); // Close the add todo screen
+              },
+              decoration: new InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)
+              ),
+            )
+          );
+        }
+      )
     );
   }
 }
