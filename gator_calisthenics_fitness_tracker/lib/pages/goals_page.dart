dd1ad4729d2bae2171fe011dc1dc4e2037070868
Angelo Reyes <angeloreyes1180@yahoo.com>
2020-10-27 11:50:27 -0700

    todo items storing on firebase now

--- gator_calisthenics_fitness_tracker/lib/pages/goals_page.dart
+++ gator_calisthenics_fitness_tracker/lib/pages/goals_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gator_calisthenics_fitness_tracker/utils/constants.dart';
 
@@ -10,10 +12,96 @@ class GoalsPage extends StatefulWidget {
 }
 
 class GoalsPageState extends State<GoalsPage> {
+
+  final firestoreInstance = FirebaseFirestore.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  
   List<String> _todoItems = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.green,
+        title: new Text('Set Goals Here')
+      ),
+      body: Center(
+      child: Container(
+        decoration: primaryBackground,
+        padding: const EdgeInsets.all(10.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('todos')
+            .snapshots(),
+          builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError)
+                return new Text('Error: ${snapshot.error}');
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return new Text('Loading...');
+                default:
+                  return new ListView(
+                    children: snapshot.data.docs
+                      .map((DocumentSnapshot document) {
+                        return new Text(
+                          document['todo_item'],
+                          //description: document['description'],
+                        );
+                    }).toList(),
+                  );
+              }
+            },
+          )),
+        ),
+        floatingActionButton: new FloatingActionButton(
+        backgroundColor: Colors.green[900],
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add task',
+        child: new Icon(
+          Icons.add,
+          color: Colors.black,
+        )
+      ),
+    );
+  }
+
+  void _pushAddTodoScreen() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              backgroundColor: Colors.green[800],
+              title: new Text('Add a new goal')
+            ),
+            body: new TextField(
+              autofocus: true,
+              onSubmitted: (val) {
+                _addTodoItem(val);
+                Navigator.pop(context); // Close the add todo screen
+              },
+              decoration: new InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)
+              ),
+            )
+          );
+        }
+      )
+    );
+  }
 
   void _addTodoItem(String task) {
     if(task.length > 0) {
+    firestoreInstance.collection("todos").add(
+    {
+      "email": auth.currentUser.email,
+      "todo_item": task,
+      "has_completed": false,
+    }).then((value){
+      print(value.id);
+    });
       setState(() => _todoItems.add(task));
     }
   }
@@ -43,83 +131,6 @@ class GoalsPageState extends State<GoalsPage> {
           ]
         );
       }
-    );
-  }
-
-  // Build the whole list of todo items
-  Widget _buildTodoList() {
-    return new ListView.builder(
-      itemBuilder: (context, index) {
-        if(index < _todoItems.length) {
-          return _buildTodoItem(_todoItems[index], index);
-        }
-      },
-    );
-  }
-
-  // Build a single todo item
-  Widget _buildTodoItem(String todoText, int index) {
-    return new CheckboxListTile(
-      onChanged: null,
-      value: false,
-      title: new Text(
-        todoText,
-        style: TextStyle(color: Colors.black),
-      ),
-      //onTap: () => _promptRemoveTodoItem(index)
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.green,
-        title: new Text('Set Goals Here')
-      ),
-      body: Container(
-        decoration: primaryBackground,
-        child: _buildTodoList(),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        backgroundColor: Colors.green[900],
-        onPressed: _pushAddTodoScreen,
-        tooltip: 'Add task',
-        child: new Icon(
-          Icons.add,
-          color: Colors.black,
-        )
-      ),
-    );
-  }
-
-  void _pushAddTodoScreen() {
-    // Push this page onto the stack
-    Navigator.of(context).push(
-      // MaterialPageRoute will automatically animate the screen entry, as well as adding
-      // a back button to close it
-      new MaterialPageRoute(
-        builder: (context) {
-          return new Scaffold(
-            appBar: new AppBar(
-              backgroundColor: Colors.green[800],
-              title: new Text('Add a new goal')
-            ),
-            body: new TextField(
-              autofocus: true,
-              onSubmitted: (val) {
-                _addTodoItem(val);
-                Navigator.pop(context); // Close the add todo screen
-              },
-              decoration: new InputDecoration(
-                hintText: 'Enter something to do...',
-                contentPadding: const EdgeInsets.all(16.0)
-              ),
-            )
-          );
-        }
-      )
     );
   }
 }
--- gator_calisthenics_fitness_tracker/lib/pages/running_tracker_page.dart
+++ gator_calisthenics_fitness_tracker/lib/pages/running_tracker_page.dart
@@ -1,5 +1,5 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:gator_calisthenics_fitness_tracker/streams/stopwatch_stream.dart';
 import 'package:gator_calisthenics_fitness_tracker/utils/constants.dart';
 
 
@@ -13,7 +13,51 @@ class RunningTrackerPage extends StatefulWidget {
 
 class _RunningTrackerStatePage extends State<RunningTrackerPage> {
 
-  var timerStream, timerSubscription, hoursStr, minutesStr, secondsStr;
+  bool flag = true;
+  Stream<int> timerStream;
+  StreamSubscription<int> timerSubscription;    
+  // String hoursStr = '00';
+  String minutesStr = '00';
+  String secondsStr = '00';
+
+  Stream<int> stopwatchStream() {
+
+  StreamController<int> streamController;
+  Timer timer;
+  Duration timerInterval = Duration(seconds: 1);
+  int counter = 0;
+
+  void stopTimer() {
+
+    if (timer != null) {
+      timer.cancel();
+      timer = null;
+      counter = 0;
+      streamController.close();
+    }
+  }
+
+  void tick(_) {
+    counter++;
+    streamController.add(counter);
+    if (!flag) {
+      streamController.close();
+    }
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(timerInterval, tick);
+  }
+
+  streamController = StreamController<int>(
+    onListen: startTimer,
+    onCancel: stopTimer,
+    onResume: startTimer,
+    onPause: stopTimer,
+  );
+
+  return streamController.stream;
+}
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +70,9 @@ class _RunningTrackerStatePage extends State<RunningTrackerPage> {
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               Text(
-                "$hoursStr:$minutesStr:$secondsStr",
+                "$minutesStr:$secondsStr",
                 style: TextStyle(
-                  fontSize: 90.0,
+                  fontSize: 70.0,
                 ),
               ),
               SizedBox(height: 30),
@@ -41,7 +85,7 @@ class _RunningTrackerStatePage extends State<RunningTrackerPage> {
                       timerStream = stopwatchStream();
                       timerSubscription = timerStream.listen((int newTick) {
                         setState(() {
-                          hoursStr = ((newTick / (60 * 60)) % 60).floor().toString().padLeft(2, '0');
+                          // hoursStr = ((newTick / (60 * 60)) % 60).floor().toString().padLeft(2, '0');
                           minutesStr = ((newTick / 60) % 60).floor().toString().padLeft(2, '0');
                           secondsStr = (newTick % 60).floor().toString().padLeft(2,'0');
                         });
@@ -51,6 +95,27 @@ class _RunningTrackerStatePage extends State<RunningTrackerPage> {
                     child: Text(
                       'START',
                       style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 40.0),
+                  RaisedButton(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                    onPressed: () {
+                      timerSubscription.cancel();
+                      timerStream = null;
+                      setState(() {
+                        // hoursStr = '00';
+                        minutesStr = '00';
+                        secondsStr = '00';
+                      });
+                    },
+                    color: Colors.red,
+                    child: Text(
+                      'RESET',
+                      style: TextStyle(
+                        color: Colors.white,
                         fontSize: 20.0,
                       ),
                     ),
