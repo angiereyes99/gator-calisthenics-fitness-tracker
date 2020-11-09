a4ad3ea8f074f8c52867b8e613c33dd9fb133f71
Angelo Reyes <angeloreyes1180@yahoo.com>
2020-11-09 12:21:12 -0800

    new UI

--- gator_calisthenics_fitness_tracker/lib/pages/login_page.dart
+++ gator_calisthenics_fitness_tracker/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_particles/particles.dart';
 import 'package:gator_calisthenics_fitness_tracker/services/google_signing_service.dart';
 import 'package:gator_calisthenics_fitness_tracker/services/main_navigation.dart';
 import 'package:gator_calisthenics_fitness_tracker/utils/constants.dart';
@@ -14,29 +15,69 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: primaryBackground,
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image(image: AssetImage("assets/logo.png"), height: 125.0,),
-              SizedBox(height: 25,),
-              Text(
-                'Gator Calisthenics Fitness Tracker', 
-                style: TextStyle(color: primaryTextColor, fontSize: 21.5, fontFamily: font),
-              ),
-              SizedBox(height: 50),
-              _signInButton(),
-            ],
+      //backgroundColor: backgroundColorDark,
+      body: Stack(
+        children: <Widget>[
+          Particles(15, primaryTextColor),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image(image: AssetImage("assets/homelogo.PNG"), height: 180.0,),
+                SizedBox(height: 10,),
+                Text(
+                  'Gator Calisthenics Fitness Tracker',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontFamily: font,
+                    color: primaryTextColor,
+                  ),
+                ),
+                Text(
+                  'Begin your calisthenics journey!',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    height: 3.5,
+                    fontWeight: FontWeight.w400,
+                    color: primaryBackgroundLight,
+                    fontFamily: font,
+                  ),
+                ),
+                SizedBox(height: 50,),
+                _signInButton(),
+              ],
+            )
           ),
-        ),
+        ],
       ),
     );
   }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Container(
+  //       child: Center(
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.center,
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: <Widget>[
+  //             Particles(20, primaryTextColor),
+  //             Image(image: AssetImage("assets/homelogo.PNG"), height: 180.0,),
+  //             SizedBox(height: 25,),
+  //             Text(
+  //               'Gator Calisthenics Fitness Tracker', 
+  //               style: TextStyle(color: primaryTextColor, fontSize: 26.5, fontFamily: font),
+  //             ),
+  //             SizedBox(height: 50),
+  //             _signInButton(),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
 
   Widget _signInButton() {
     return OutlineButton(
@@ -70,6 +111,7 @@ class _LoginPageState extends State<LoginPage> {
                 'Sign in with Google',
                 style: TextStyle(
                   fontSize: 20,
+                  fontFamily: font,
                   color: primaryTextColor,
                 ),
               ),
